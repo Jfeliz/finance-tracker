8be02fe461ff013b36b194e27e87ce80d829f3a3
Jackniel <w.jackniel.fn@gmail.com>
2019-01-01 01:22:30 +0000

    Stock model created, users controller with my_portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  root 'users#my_portfolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'my_portfolio', to: 'user#my_portfolio'
+  
 end
