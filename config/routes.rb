c2f3151a456b9af1d8764558419a1856914aaf7c
Jackniel <w.jackniel.fn@gmail.com>
2019-01-03 23:29:37 +0000

    Stock search WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root 'users#my_portfolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get 'my_portfolio', to: 'user#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
   
 end
